7d6ab49a45fd37d9a3be139fa83439af9f940902
avadhut86 <avipitale2003@gmail.com>
2024-10-26 18:34:54 +0530

    Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Main_Page from "./Main_Page";
 import { Route, Routes } from "react-router-dom";
 import About from "./About";
 import PokeDescription from "./PokeDescription";
-function App() {
+import  Contact  from "./Contact";
+function App() { 
 
   return (
     <>
-    <Routes>
+    <Routes>  //switch also can write
     <Route path='/' element={<Main_Page />} />
     <Route path='/about' element={<About/>} />
-    <Route path='/:name' element={<PokeDescription/>}/>
+    <Route path='/pokemon/:name' element={<PokeDescription/>} />
+    <Route path='/contact' element={<Contact/>} />
     </Routes>
     </>
   );
--- src/PokeDescription.jsx
+++ src/PokeDescription.jsx
@@ -1,15 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import bulbasaur from "./assets/pokemon/1.png";
-import grass from "./assets/icons/grass.svg";
 import { Search } from "lucide-react";
 import { useParams } from 'react-router-dom';
 
 const PokeDescription = () => {
+  const getBackgroundColor = (type) => {
+    switch (type) {
+      case "fire":
+        return "#ff7402";
+      case "grass":
+        return "#33a165";
+      case "water":
+        return "#58abf6";
+      case "ghost":
+        return "#705898";
+      case "steel":
+        return "#00858a";
+      case "psychic":
+        return "#c90086";
+      case "ground":
+        return "#c90089";
+      case "ice":
+        return "#70deff";
+      case "flying":
+        return "#5d4e75";
+      case "electric":
+        return "#facd4b";
+      case "normal":
+        return "#753845";
+      case "poison":
+        return "#7e0058";
+      case "rock":
+        return "#6e1a00";
+      case "fighting":
+        return "#634136";
+      case "dark":
+        return "#272625";
+      case "bug":
+        return "#6e1a00";
+      case "dragon":
+        return "#00c431";
+      case "fairy":
+        return "#d31c81";
+      case "shadow":
+        return "#29292c";
+
+      default:
+        return "#757575";
+    }
+  };
+
+  
     const { name } = useParams(); 
     const [pokemonData, setPokemonData] = useState(null) 
-
-    
-    const imagePath = `/assets/pokemon/${name.toLowerCase()}.png`;
 
     console.log(pokemonData)
 
@@ -20,11 +62,20 @@ const PokeDescription = () => {
         .catch((error) => console.error(error));
     }, [name])
 
- 
+    if (!pokemonData) {
+      return <h1>Loading...</h1>;
+    }
+    const pokemonId = pokemonData.id;
+    const imagePath =  `/assets/pokemon/${pokemonId}.png`;
+    
+    const pokemonType = pokemonData.types[0].type.name;
+    const typeImg = `/assets/icons/${pokemonType}.svg`;
+
+    const backgroundColor = getBackgroundColor(pokemonData.types[0].type.name); 
     
   return (
     <>
-       { pokemonData && <div className="container">
+       { pokemonData && <div className="container"  style={{ backgroundColor}}>
         <header>
           <div className="search-container">
             <Search className="search-icon" />
@@ -36,22 +87,15 @@ const PokeDescription = () => {
           </div>
         </header>
 
-        <nav>
-          <a href="#">Bulbasaur</a>
-          <a href="#">Ivysaur</a>
-          <a href="#">Venusaur</a>
-          <a href="#">Charmander</a>
-        </nav>
-
         <div className="main">
-          <h1>{pokemonData.id}</h1>
+          <h1>#0{pokemonData.id}</h1>
           <div className="img-sideinfo">
             <div className="grass">
-              <img src={grass} alt="" />
+              <img src={typeImg} alt="" />
 
               <div className="name">
-                <p> {pokemonData.types[0].type.name} </p>
-                <h2>{pokemonData.name}</h2>
+                <p> {pokemonData.types[0].type.name.toUpperCase()} </p>
+                <h2>{pokemonData.name.toUpperCase()}</h2>
               </div>
             </div>
 
@@ -76,31 +120,31 @@ const PokeDescription = () => {
           </div>
           <div className="para">Hp</div>
           <div className="bar">
-            <p className="range"></p>
+            <p className="range" style={{ width:`${pokemonData.stats[0].base_stat}%`}}></p>
           </div>
           <div className="number"> {pokemonData.stats[0].base_stat} </div>
 
           <div className="para">Attack</div>
           <div className="bar">
-            <p className="attack"></p>
+            <p className="attack" style={{ width:`${pokemonData.stats[1].base_stat}%`}}></p>
           </div>
           <div className="number"> {pokemonData.stats[1].base_stat} </div>
 
           <div className="para">Defense</div>
           <div className="bar">
-            <p className="defnse"></p>
+            <p className="defnse"style={{ width:`${pokemonData.stats[2].base_stat}%`}}></p>
           </div>
           <div className="number"> {pokemonData.stats[2].base_stat}</div>
 
           <div className="para">Special-Defense</div>
           <div className="bar">
-            <p className="sp-defnse"></p>
+            <p className="sp-defnse"style={{ width:`${pokemonData.stats[4].base_stat}%`}}></p>
           </div>
           <div className="number"> {pokemonData.stats[4].base_stat} </div>
 
           <div className="para">Speed</div>
           <div className="bar">
-            <p className="speed"></p>
+            <p className="speed"style={{ width:`${pokemonData.stats[5].base_stat}%`}}></p>  {/*max = "200"*/} 
           </div>
           <div className="number"> {pokemonData.stats[5].base_stat} </div>
         </div>
@@ -112,3 +156,5 @@ const PokeDescription = () => {
 }
 
 export default PokeDescription
+
+
