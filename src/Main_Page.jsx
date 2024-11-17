efaa92f0c61ef31e4b4c9620535d864e73876458
UmeshPalival <umeshpalival@gofynd.com>
2024-11-17 17:35:11 +0530

    Css changes for the search bar

--- src/Main_Page.jsx
+++ src/Main_Page.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import Card from './Card'; 
-import axios from 'axios';
+import Card from "./Card";
+import axios from "axios";
 import Loading from "./Loading";
 import { useNavigate } from "react-router-dom";
 
 const Main_Page = () => {
   const navigate = useNavigate();
-  const [pokeData, setpokeData] = useState([]); 
+  const [pokeData, setpokeData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
   const [nextUrl, setnextUrl] = useState();
   const [prevUrl, setprevUrl] = useState();
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
   const pokeFun = async () => {
@@ -44,7 +44,7 @@ const Main_Page = () => {
   };
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleSearch();
     }
   };
@@ -82,27 +82,26 @@ const Main_Page = () => {
     <>
       <nav className="navigation">
         <img src="/assets/pokemon_navbar1.png" alt="" />
-          <input
-            type="text"
-            placeholder="Search Here For Pokemon"
-            className="search-input"
-            value={searchInput}
-            onChange={handleInputChange}
-            onKeyPress={handleKeyPress}
-          />
-          {suggestions.length > 0 && (
-            <ul className="suggestions-list">
-              {suggestions.map((pokemon) => (
-                <li
-                  key={pokemon.name}
-                  onClick={() => handleSuggestionClick(pokemon.name)}
-                >
-                  {pokemon.name}
-                </li>
-              ))}
-            </ul>
-          )}
-
+        <input
+          type="text"
+          placeholder="Search Here For Pokemon"
+          className="search-input"
+          value={searchInput}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+        />
+        {suggestions.length > 0 && (
+          <ul className="suggestions-list">
+            {suggestions.map((pokemon) => (
+              <li
+                key={pokemon.name}
+                onClick={() => handleSuggestionClick(pokemon.name)}
+              >
+                {pokemon.name}
+              </li>
+            ))}
+          </ul>
+        )}
       </nav>
       <div className="card-mainpage">
         <Card pokemon={pokeData} loading={loading} />
